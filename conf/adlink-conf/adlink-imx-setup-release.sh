8919978907818d1c36c6d5e3efbcf759ed8cf064
po.cheng <po.cheng@adlinktech.com>
2021-08-27 15:45:39 +0800

    setup: renew pat
    Signed-off-by: po.cheng <po.cheng@adlinktech.com>

--- conf/adlink-conf/adlink-imx-setup-release.sh
+++ conf/adlink-conf/adlink-imx-setup-release.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
 TOKEN_PRE="ghp_"
-TOKEN_POST="n4138GgHZwaS7MChjf83NeQD2E7iUt01u9po"
-
+TOKEN_POST="iGvgyyrnRFoA71UrF0N12vm6pqEIFi0xgPs7"
 CWD=$(pwd)
 PROGNAME="$CWD/imx-setup-release.sh"
 
