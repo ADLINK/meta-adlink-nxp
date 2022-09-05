59aee684b057b3f25cff57de51a45690d693c71e
AdlinkCCoE <104615818+AdlinkCCoE@users.noreply.github.com>
2022-09-05 11:38:05 +0530

    Access Token updated

--- conf/adlink-conf/adlink-imx-setup-release.sh
+++ conf/adlink-conf/adlink-imx-setup-release.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 TOKEN_PRE="ghp_"
-TOKEN_POST="iGvgyyrnRFoA71UrF0N12vm6pqEIFi0xgPs7"
+TOKEN_POST="DiFPUbMC2PjG06sFvpuDrsBdJAH5bO0bSQd9"
 CWD=$(pwd)
 PROGNAME="$CWD/imx-setup-release.sh"
 
