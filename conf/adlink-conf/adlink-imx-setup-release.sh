db6262723b220273d0abc6b25fe6c6dbcb906229
po.cheng <po.cheng@adlinktech.com>
2024-01-17 09:10:02 +0800

    machine: setup: append meta-nxp-desktop bblayer and add imx-image-desktop.bbappend and ubuntu-base_%.bbappend to BBMASK
    Signed-off-by: po.cheng <po.cheng@adlinktech.com>

--- conf/adlink-conf/adlink-imx-setup-release.sh
+++ conf/adlink-conf/adlink-imx-setup-release.sh
@@ -4,6 +4,8 @@ TOKEN_PRE="ghp_"
 TOKEN_POST="DiFPUbMC2PjG06sFvpuDrsBdJAH5bO0bSQd9"
 CWD=$(pwd)
 PROGNAME="$CWD/imx-setup-release.sh"
+DISTRO_NAME="$DISTRO"
+MACHINE_NAME="$MACHINE"
 
 if [ -z "$BUILD" ]; then
 	BUILD="build"
@@ -20,4 +22,17 @@ fi
 
 echo "PA_USER ?= \"adlink-guest\"" >> ./conf/local.conf
 echo "PA_TOKEN ?= \""${TOKEN_PRE}${TOKEN_POST}"\"" >> ./conf/local.conf
+
+if [ "${DISTRO_NAME}" = "imx-desktop-xwayland" -a -d ../sources/meta-nxp-desktop ]; then
+	echo "BBLAYERS += \"\${BSPDIR}/sources/meta-nxp-desktop\"" >> ./conf/bblayers.conf
+fi
+
+if [ ! "${DISTRO_NAME}" = "adlink-rtedge-desktop" ]; then
+	echo "BBMASK += \"rteval_%.bbappend\"" >> ./conf/local.conf
+fi
+
+if [ ! -d ../sources/meta-nxp-desktop ]; then
+	echo "BBMASK += \"imx-image-desktop.bbappend\"" >> ./conf/local.conf
+	echo "BBMASK += \"ubuntu-base_%.bbappend\"" >> ./conf/local.conf
+fi
 
