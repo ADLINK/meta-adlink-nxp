ba0a08aca6e105a9215baefc54cb94ce33ecb622
po.cheng <po.cheng@adlinktech.com>
2026-02-04 14:27:32 +0800

    SP2-IMX8MP: Conf: Modify for Real-Time-Edge
    Signed-off-by: po.cheng <po.cheng@adlinktech.com>

--- conf/adlink-conf/adlink-imx-setup-release.sh
+++ conf/adlink-conf/adlink-imx-setup-release.sh
@@ -29,7 +29,7 @@ fi
 
 if [ -d ../sources/meta-nxp-desktop ]; then
 	if ! grep -q meta-nxp-desktop ./conf/bblayers.conf; then
-		echo "BBLAYERS += \"\${BSPDIR}/sources/meta-nxp-desktop\"" >> ./conf/bblayers.conf
+		hook_in_layer meta-nxp-desktop
 	fi
 	if [ "${DISTRO_NAME}" = "imx-desktop-xwayland" ]; then
 		cat ../sources/meta-adlink-nxp/conf/adlink-conf/$MACHINE/desktop.local.conf.append >> ./conf/local.conf
@@ -43,10 +43,6 @@ fi
 
 if [ "$MULTILIB" = "lib32" ]; then
 	cat ../sources/meta-adlink-nxp/conf/adlink-conf/$MACHINE/multilib.local.conf.append >> ./conf/local.conf
-fi
-
-if [ ! "${DISTRO_NAME}" = "adlink-rtedge-desktop" ]; then
-	echo "BBMASK += \"rteval_%.bbappend\"" >> ./conf/local.conf
 fi
 
 # nxp-wlan-sdk bbappend is not buildable, mask it
@@ -70,7 +66,7 @@ if [ "$NEUTRON_EIQ_DEMO" = "1" ]; then
     # Set Neutron support in local.conf
     echo "NEUTRON_SUPPORT = \"1\"" >> ./conf/local.conf
     
-   # Add packages for Neutron EIQ demo
+    # Add packages for Neutron EIQ demo
     echo 'IMAGE_INSTALL:append = " alsa-lib-dev eiq-genai-flow-dep"' >> ./conf/local.conf
 
     # Mask unwanted bbappend for Neutron support
@@ -80,47 +76,58 @@ if [ "$NEUTRON_EIQ_DEMO" = "1" ]; then
 
     echo "BBMASK += \".*meta-eiq-genai-flow.*/imx-image-full-eiq-genai-flow-dep.bb\"" >> ./conf/local.conf
     
-     #Automatically add scarthgap compatibility to layer.conf
+    #Automatically add scarthgap compatibility to layer.conf
     export BSPDIR="$(pwd)"
-# Path to the layer.conf file
+    # Path to the layer.conf file
     LCONF="${BSPDIR}/../sources/dm-eiq-genai-flow-demonstrator/meta-eiq-genai-flow/conf/layer.conf"
 
-# Check if file exists
+    # Check if file exists
     if [ -f "$LCONF" ]; then
-    # Check if the line exists
-    if grep -q 'LAYERSERIES_COMPAT_meta-eiq-genai-flow' "$LCONF"; then
-        # Only add 'scarthgap' if it's not already there
-        if ! grep -q 'scarthgap' "$LCONF"; then
-            sed -i 's/\(LAYERSERIES_COMPAT_meta-eiq-genai-flow *= *"[^"]*\)"/\1 scarthgap"/' "$LCONF"
-            echo "scarthgap added to existing LAYERSERIES_COMPAT"
+        # Check if the line exists
+        if grep -q 'LAYERSERIES_COMPAT_meta-eiq-genai-flow' "$LCONF"; then
+            # Only add 'scarthgap' if it's not already there
+            if ! grep -q 'scarthgap' "$LCONF"; then
+                sed -i 's/\(LAYERSERIES_COMPAT_meta-eiq-genai-flow *= *"[^"]*\)"/\1 scarthgap"/' "$LCONF"
+                echo "scarthgap added to existing LAYERSERIES_COMPAT"
+            else
+                echo "scarthgap already present, no changes made"
+            fi
         else
-            echo "scarthgap already present, no changes made"
+            # Add a new line if LAYERSERIES_COMPAT is missing
+            echo '' >> "$LCONF"
+            echo 'LAYERSERIES_COMPAT_meta-eiq-genai-flow = "scarthgap"' >> "$LCONF"
+            echo "Added new LAYERSERIES_COMPAT line with scarthgap"
         fi
     else
-        # Add a new line if LAYERSERIES_COMPAT is missing
-        echo '' >> "$LCONF"
-        echo 'LAYERSERIES_COMPAT_meta-eiq-genai-flow = "scarthgap"' >> "$LCONF"
-        echo "Added new LAYERSERIES_COMPAT line with scarthgap"
+        echo "$LCONF not found. Skipping patch."
     fi
-else
-    echo "$LCONF not found. Skipping patch."
-fi
-
 fi
 
 
 # hook the nxp hab boot stuff
 if [ "$HAB" = "1" ]; then
-	echo "IMAGE_FEATURES[validitems] += \"hab\"" >> ./conf/local.conf
-	echo "EXTRA_IMAGE_FEATURES:append = \" hab \"" >> ./conf/local.conf
-	hook_in_layer meta-adlink-nxp/meta-adlink-hab
-	case "$MACHINE" in
-	*mx8m*)
-		echo "HAB_VER = \"habv4\"" >> ./conf/local.conf
-		;;
-	*)
-		echo "HAB_VER = \"ahab\"" >> ./conf/local.conf
-		;;
-	esac
+    echo "IMAGE_FEATURES[validitems] += \"hab\"" >> ./conf/local.conf
+    echo "EXTRA_IMAGE_FEATURES:append = \" hab \"" >> ./conf/local.conf
+    hook_in_layer meta-adlink-nxp/meta-adlink-hab
+    case "$MACHINE" in
+    *mx8m*)
+        echo "HAB_VER = \"habv4\"" >> ./conf/local.conf
+        ;;
+    *)
+        echo "HAB_VER = \"ahab\"" >> ./conf/local.conf
+        ;;
+    esac
+fi
+
+# hook the real-time-edge meta-layer
+if [ "$RTEDGE" = "1" ]; then
+    if [ -d ../sources/meta-real-time-edge ]; then
+        if ! grep -q "meta-real-time-edge" ./conf/bblayers.conf; then
+            hook_in_layer meta-real-time-edge
+        fi
+        if [ -f ../sources/meta-adlink-nxp/conf/adlink-conf/$MACHINE/real-time-edge.inc ]; then
+            echo "include \${BSPDIR}/sources/meta-adlink-nxp/conf/adlink-conf/$MACHINE/real-time-edge.inc" >> ./conf/local.conf
+        fi
+    fi
 fi
 
